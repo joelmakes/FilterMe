6cbcd28d327fed3943f6d2c984303ac485647712
joelmakes <thejoelg0824@gmail.com>
2026-08-20 08:26:23 -0700

    Fix Windows app user model ID setting for better taskbar integration

--- App.py
+++ App.py
@@ -10,6 +10,10 @@ if __name__ == "__main__":
 
     print("version:", Version)
 
+    if sys.platform == "win32":
+        import ctypes
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("FilterMe.App.1.0.0")
+
     app = QApplication(sys.argv)
 
     window = FilterMe()
